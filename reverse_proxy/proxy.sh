83d653d9ce4880509cbcdfbdb8646e6ee258bd3a
Igor Marchenko <igor.marchenko@dominodatalab.com>
2020-05-15 00:41:13 -0400

    Nightly change

--- reverse_proxy/proxy.sh
+++ reverse_proxy/proxy.sh
@@ -9,4 +9,4 @@ fi
  
 yum install nginx -y
 sed -iE "s#8888#$REVERSE_PROXY_PORT#g" ${SASDS_SCRIPT_DIR}/nginx.conf
-sed -iE "s#function start_viya .*#function start_viya { sudo SAS_LOGS_TO_DISK=\$SAS_LOGS_TO_DISK su --session-command '/opt/sas/viya/home/bin/entrypoint \&' root; until \$(curl --output /dev/null --silent --head --fail http://localhost:7080/SASStudio); do sleep 3; done; sudo /usr/sbin/nginx -c \${SASDS_SCRIPT_DIR}/nginx.conf; echo -e '\\\n\\\nStarted reverse proxy server...\\\n\\\n'; while true; do :; done; }#g" ${SASDS_SCRIPT_DIR}/start
+sed -iE "s#function start_viya .*#function start_viya { sudo SAS_LOGS_TO_DISK=\$SAS_LOGS_TO_DISK su --session-command '/opt/sas/viya/home/bin/entrypoint \&' root; until \$(curl --output /dev/null --silent --head --fail http://localhost:7080/SASStudio); do sleep 3; done; sudo /usr/sbin/nginx -c \${SASDS_SCRIPT_DIR}/nginx.conf; echo -e '\\\n\\\nSAS Studio is now running\\\nTo get started, please visit <SAS Studio Host>/SASStudio/start.html in your web browser.\\\n\\\n'; while true; do :; done; }#g" ${SASDS_SCRIPT_DIR}/start
